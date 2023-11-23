27db01a18f664b160276d772c73dba0ebb7de38d
ezebastian <sebastian.ezequiel.elcontar.1991@gmail.com>
2023-11-23 15:25:09 -0300

    Hooks

--- Front/RickandMorty-PI/src/components/nav/Nav.jsx
+++ Front/RickandMorty-PI/src/components/nav/Nav.jsx
@@ -1,19 +1,22 @@
 import { NavLink } from "react-router-dom";
-import SearchBar from "/src/components/searchbar/SearchBar";
-
+import SearchBar from "../searchbar/SearchBar";
 
 export default function Nav(props) {
 
-   return (
-      <div>
-          <NavLink to="/home">
-             <button>Home</button>
-          </NavLink>
-          <NavLink to="/about">
-             <button>About</button>
-          </NavLink>
-          <hr />
-         <SearchBar onSearch={props.onSearch} />
-      </div>
-   );
- }
+  return (
+     <div>
+         <NavLink to="/home">
+            <button>Home</button>
+         </NavLink>
+         <NavLink to="/favorites">
+            <button>Favorites</button>
+         </NavLink>
+         <NavLink to="/about">
+            <button>About</button>
+         </NavLink>
+         <button onClick={props.logout} >Logout ❌</button>
+         <hr />
+        <SearchBar onSearch={props.onSearch} />
+     </div>
+  );
+}
