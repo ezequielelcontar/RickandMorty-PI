32a9dd7a0f1514885346f25621fc77bf130eefcd
ezebastian <sebastian.ezequiel.elcontar.1991@gmail.com>
2023-12-15 21:52:04 -0300

    SQL

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,13 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+//* Sincronizar la instancia de "Sequelize" al servidor
+conn.sync({ force: false }) //* Retrona promesa !!!
+   .then(() => {
+      server.listen(PORT, () => {
+         console.log('Server raised in port: ' + PORT);
+      });
+   })
+   .catch(error => console.log(error.message))
+
